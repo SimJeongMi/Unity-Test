29d96792e93272ae367d3364fca048a3850c05e1
Harin Lee(이하린) <86524081+riiine@users.noreply.github.com>
2022-11-18 11:43:10 +0900

    스피어, 머티리얼 생성
    스피어, 머티리얼 생성

--- Test/Assets/HR/1.Scripts/Sphere.cs
+++ Test/Assets/HR/1.Scripts/Sphere.cs
@@ -7,7 +7,7 @@ public class Sphere : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Sphere");
     }
 
     // Update is called once per frame
